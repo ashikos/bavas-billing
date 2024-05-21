9c9dfa0d8a117f938afef11119f0e7cf6082fee2
ashik <ashik.os@cied.in>
2024-05-21 14:50:11 +0530

    error1

--- bavas/bavas/settings.py
+++ bavas/bavas/settings.py
@@ -180,9 +180,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations of static files
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
