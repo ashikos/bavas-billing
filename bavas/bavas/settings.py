96ec891f6cdb460dfe794d3d4e97349fcdeb58c5
ashik <ashik.os@cied.in>
2024-09-05 03:31:25 +0530

    static added

--- bavas/bavas/settings.py
+++ bavas/bavas/settings.py
@@ -203,18 +203,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#
-# # Additional locations of static files
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-
-STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# STATIC_DIR=os.path.join(BASE_DIR,'static')
+#
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
